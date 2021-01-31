508331a3f6cbbc535935a0159659bd397a4a6dc3
akmal2409 <akmalalikhujaev@gmail.com>
2021-01-31 21:03:54 +0100

    Opened an API endpoint to verify account

--- talcistore-spring/src/main/java/tech/talci/talcistorespring/controllers/AuthController.java
+++ talcistore-spring/src/main/java/tech/talci/talcistorespring/controllers/AuthController.java
@@ -3,10 +3,7 @@ package tech.talci.talcistorespring.controllers;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import tech.talci.talcistorespring.dto.RegisterRequest;
 import tech.talci.talcistorespring.services.AuthService;
 
@@ -22,5 +19,11 @@ public class AuthController {
     public ResponseEntity<String> signUp(@RequestBody RegisterRequest registerRequest) {
         authService.singUp(registerRequest);
         return new ResponseEntity<>("You were successfully registered", HttpStatus.CREATED);
+    }
+
+    @GetMapping("/verify-account/{token}")
+    public ResponseEntity<String> verifyAccount(@PathVariable String token) {
+        authService.verifyAccount(token);
+        return new ResponseEntity<>("Account was successfully activated!", HttpStatus.OK);
     }
 }
