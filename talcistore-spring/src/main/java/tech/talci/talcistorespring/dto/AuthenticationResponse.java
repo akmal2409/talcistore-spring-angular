682b4f33e05c3babe5ae7cfd60edd1de6edbeb91
akmal2409 <akmalalikhujaev@gmail.com>
2021-02-01 18:26:20 +0100

    Added two DTO object LoginRequest and AuthenticationResponse

--- talcistore-spring/src/main/java/tech/talci/talcistorespring/dto/AuthenticationResponse.java
+++ talcistore-spring/src/main/java/tech/talci/talcistorespring/dto/AuthenticationResponse.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class LoginRequest {
+public class AuthenticationResponse {
+    private String token;
     private String username;
-    private String password;
 }
