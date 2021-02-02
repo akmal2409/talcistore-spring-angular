6500e3a1e3ea698f2b0396b60d26557268b19bf7
akmal2409 <akmalalikhujaev@gmail.com>
2021-02-02 12:48:03 +0100

    Added validation for Product Dto

--- talcistore-spring/src/main/java/tech/talci/talcistorespring/dto/ProductDto.java
+++ talcistore-spring/src/main/java/tech/talci/talcistorespring/dto/ProductDto.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -14,13 +16,23 @@ import java.time.LocalDate;
 public class ProductDto {
 
     private Long id;
+
     @NotEmpty(message = "Product name is required")
     private String productName;
+
+    @NotEmpty(message = "Product description is required")
     private String description;
+
+    @DecimalMin(value = "1.0", inclusive = true, message = "Price per unit must be greater than {value}")
     private BigDecimal pricePerUnit;
+    @NotEmpty(message = "Category id is required")
     private Long categoryId;
+    @NotEmpty(message = "Country of origin is required")
     private String countryOfOrigin;
+    @NotEmpty(message = "Producer is required")
     private String producer;
+
+    @Min(value = 0, message = "Amount in stock must be greater than {value}")
     private Long leftInStock;
     private Double rating;
     private Long orderCount;
