d529d133fd5fc5d274dd43ee46622c6706854aae
akmal2409 <akmalalikhujaev@gmail.com>
2021-02-09 21:21:43 +0100

    Added RestExceptionHandler

---
+++ talcistore-spring/src/main/java/tech/talci/talcistorespring/controllers/RestExceptionHandler.java
@@ -0,0 +1,46 @@
+package tech.talci.talcistorespring.controllers;
+
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import tech.talci.talcistorespring.dto.errorHandlers.ApiError;
+
+import static org.springframework.core.Ordered.HIGHEST_PRECEDENCE;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.FORBIDDEN;
+
+@Order(HIGHEST_PRECEDENCE)
+@ControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatus status,
+                                                                  WebRequest request) {
+        String error = "Malformed JSON request";
+        return buildResponseEntity(new ApiError(BAD_REQUEST, error, ex));
+    }
+
+    @ExceptionHandler(DisabledException.class)
+    protected ResponseEntity<Object> handleUserDisabled(DisabledException ex,
+                                                        HttpHeaders headers,
+                                                        HttpStatus status,
+                                                        WebRequest request) {
+        String error = "Account is disabled or was not verified";
+        ApiError apiError = new ApiError(FORBIDDEN, error, ex);
+        return buildResponseEntity(apiError);
+    }
+
+
+    private ResponseEntity<Object> buildResponseEntity(ApiError apiError) {
+        return new ResponseEntity<>(apiError, apiError.getStatus());
+    }
+}
